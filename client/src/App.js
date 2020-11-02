888fdb65d61c7e1ec6151a2fd480e0f5556b7580
Aiden Szeto <aszeto35@gmail.com>
2020-11-02 10:44:04 -0800

    main header

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Countdown from './components/countdown/countdown'
 import Email from './components/email/email'
+import './main.css'
 
 const App = function () {
 	return (
     <div>
-		<Countdown timeTillDate="12 25 2020, 12:00 am" timeFormat="MM DD YYYY, h:mm a" />
-		<br />
-		<Email />
+		<div className='container1'>
+			<h2 className='center'> The Future of Flexible Rentals </h2>
+			<h3 className='center'> Register now to be notified when we officially release! </h3>
+			<Email />
+			<Countdown timeTillDate="12 25 2020, 12:00 am" timeFormat="MM DD YYYY, h:mm a" />
+		</div>
 	</div>
 	);
 };
