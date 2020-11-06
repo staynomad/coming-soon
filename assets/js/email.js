04895ba7892b4ece03f510f79fd873d9d02646ef
Seamus Le <seamust.le@gmail.com>
2020-11-05 19:57:29 -0800

    added preventDefault() to email.js fetch call.  Added header Content-Type to fetch request

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,24 +1,29 @@
-window.onload = function() {
+window.onload = function () {
     document.getElementById("button").addEventListener("click",
-        function() {
+        function (event) {
+            event.preventDefault();
             const data = { "email": document.getElementById('email').value };
             fetch('https://vhomes-coming-soon-backend.herokuapp.com/users', {
-              method: 'POST',
-              body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(data),
             })
-            .then(() => {
-                alert("Email has been submitted!")
-                window.location.reload()
-            })
-            .catch((err) => {
-                console.log(err)
-                if (err.response) {
-                    alert(err.response.data.errors[0])
-                }
-                else {
-                    // alert("Error processing entry. Please try again.")
-                }
-            });
+                .then(() => {
+                    console.log(JSON.stringify(data))
+                    alert("Email has been submitted!")
+                    window.location.reload()
+                })
+                .catch((err) => {
+                    console.log(err)
+                    if (err.response) {
+                        alert(err.response.data.errors[0])
+                    }
+                    else {
+                        // alert("Error processing entry. Please try again.")
+                    }
+                });
         }
     );
 }
